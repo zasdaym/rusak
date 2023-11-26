9ca8d8ae2a8e3bc11ee77e5128925e57c53cdc88
Zasda Yusuf Mikail <zasdaym@gmail.com>
2023-11-26 10:09:19 +0700

    Annotate exception with user information

--- web/app/bad/page.tsx
+++ web/app/bad/page.tsx
@@ -3,7 +3,14 @@ import { captureException } from "@sentry/react"
 async function getData() {
   const response = await fetch('http://localhost:8080/bad')
   if (!response.ok) {
-    captureException(new Error('Bad response'))
+    captureException(
+      new Error('Bad response from server'),
+      {
+        user: {
+          id: 666,
+          email: 'zasdaym@gmail.com' // Pretend this is extracted from the session,
+        }
+      })
   }
   const data = await response.text()
   return data
